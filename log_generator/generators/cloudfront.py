74adaf63ffb77dcdb8986ffcb4fb9e9c44280ba1
Georgios Soteriou <Georgios.Soteriou@netacea.com>
2021-04-28 18:04:28 +0100

    fuzzying values cloudfront

--- log_generator/generators/cloudfront.py
+++ log_generator/generators/cloudfront.py
@@ -14,10 +14,10 @@ class Cloudfront(LogRender):
         Returns:
             str: Cloudfront Log Line format.
         """
-        x_edge_location = "CDG3-C1"
+        x_edge_location = "Casdf3-C1"
         cloudfront_host = "d1yn713.cloudfront.net"
         x_edge_result_type = "Miss"
-        x_edge_request_id = "S7rh9sTk8_hC6HUOjWRywP4s5L4fMxnRkMVpdVeyg9q_JZd-8P49wQ=="
+        x_edge_request_id = "S7rh9sTk8_hC6HUOjWRywP4s5Leyg9q_JZd-8P49wQ=="
         x_host_header = "sports.dummydomain.com"
         x_forwarded_for = "-"
         ssl_protocol = "TLSv1.2"
@@ -26,4 +26,4 @@ class Cloudfront(LogRender):
         fle_status = "-"
         fle_encrypted_fields = "-"
         return f"""!!!{self.timestamp_format}!!! {x_edge_location} {d["transfer_size"]} {d["ip_address"]} {d["http_method"]} {cloudfront_host} {d["uri_path"]} {d["http_status"]} {d["referer"]} {d["user_agent"]} {d["uri_query_params"]} {d["user_name"]} {x_edge_result_type} {x_edge_request_id} {x_host_header} {d["http_protocol"]} {d["transfer_size"]} {d["request_time"]} {x_forwarded_for} (ssl_protocol) {ssl_cipher} {x_edge_response_result_type} {d["http_protocol"]} {fle_status} {fle_encrypted_fields}
-""" # noqa: E501  
+""" # noqa: E501
