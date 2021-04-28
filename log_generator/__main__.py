e9d928a6d2c2460d49279dd80703511a420176b5
Daniel Loader <daniel.loader@netacea.com>
2021-04-28 14:29:27 +0000

    Add http client Implement S3 client Update requirements.txt

--- log_generator/__main__.py
+++ log_generator/__main__.py
@@ -2,7 +2,6 @@ import sys
 from enum import Enum
 from typing import Optional
 
-from typer.params import Option
 from web import Web
 
 import typer
---
+++ log_generator/streams/http.py
@@ -0,0 +1,37 @@
+
+from streams.base import Output
+import httpx
+
+class HTTP(Output):
+    def __init__(self, url: str, rate: int = 1):
+        """Synchronous HTTP client for submitting logs.
+
+        Args:
+            url (str): URL of endpoint to POST to.
+            rate (int, optional): Submissions per second rate limiter. Defaults to 1.
+        """
+        super().__init__(rate=rate)
+        self.url = url
+
+    def __enter__(self):
+        self.client = httpx.Client(http2=True)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.client.close()
+
+    def send(self, logline: str):
+        data = {
+            "Request": logline,
+            "TimeLocal": "2020-09-25 16:30:00:00 +0000",
+            "RealIp": "1.1.1.1",
+            "UserAgent": "UserAgent Value",
+            "Status": "200",
+            "RequestTime": "9",
+            "BytesSent": "100",
+            "NetaceaUserIdCookie": "NzQ5NTkyMDBmNDc5NmJkYzcwZmQ5YzgxYzg4MjI3NTcwZWYwODVkYTc0NGI4YmM3MTFmNjE3YWQzM2UwZTgxMQ==_/@#/1596112893_/@#/m92recxp96jt0vfh_/@#/211;",
+            "NetaceaMitigationApplied": "ip_allow",
+            "Referer": "-"
+        }
+        response = self.client.post(self.url, data=data)
+    
