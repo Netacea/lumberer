3666eb09f720068a1d63869129e7a6f832f3b178
Rebecca Davey <rebecca.davey@netacea.com>
2021-04-28 14:03:35 +0100

    above but nicer use of super args

--- log_generator/generators/base.py
+++ log_generator/generators/base.py
@@ -4,9 +4,9 @@ import sys
 
 
 class LogRender:
-    def __init__(self, iterations: int = 1, seed: int = 4321):
-        self.timestamp_format = "%Y-%m-%dT%H:%M:%S"
+    def __init__(self, iterations: int = 1, seed: int = 4321, timestamp_format: str = "%Y-%m-%dT%H:%M:%S"):
         self.iterations = iterations
+        self.timestamp_format = timestamp_format
         self.fake = Faker()
         Faker.seed(seed)
 
