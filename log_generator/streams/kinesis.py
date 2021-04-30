cf5b3013371527da97fac5d89ae0ed8528b4a818
Georgios Soteriou <Georgios.Soteriou@netacea.com>
2021-04-30 14:19:04 +0100

    kinesis sink

---
+++ log_generator/streams/kinesis.py
@@ -0,0 +1,58 @@
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime, timezone
+from streams.base import Output
+
+class Kinesis(Output):
+    def __init__(
+        self, topic: str, rate: int = None, schedule: dict = None
+    ):
+        """Kafka sink using the kafka=python library.
+
+        Args:
+            broker (list): List of brokers to connect to.
+            topic (str): Topic to produce the messages to.
+            rate (int, optional): Rate per second to send. Defaults to None.
+            schedule (dict, optional): Scheduled rate limits. Defaults to None.
+        """
+        super().__init__(rate=rate, schedule=schedule)
+        self.topic = topic
+        self.client = boto3.client("kinesis")
+
+    def __enter__(self, buffer_size=500):
+        self.buffer = []
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if len(self.buffer) > 0:
+            self.write()
+
+    def _send(self, logline: str):
+        """Send proxy to write to a buffer until the size is reached.
+
+        Args:
+            logline (str): Generated log line to be sent.
+        """
+        self.buffer.append({"Data": logline.encode("utf-8"), "PartitionKey": logline[:10]})
+        if len(self.buffer) >= self.buffer_size:
+            self.write()
+
+    def write(self):
+        """Write the buffer to Kinesis.
+
+        Raises:
+            e: Exception if we fail to send a batch of data. 
+        """
+        try:
+            response = self.client.put_records(
+                Records=self.buffer,
+                StreamName=self.topic
+            )
+            if "FailedRecordCount" in response and response["FailedRecordCount"] > 0:
+                assert False, f"There were {response["FailedRecordCount"]} records failed to go to {self.topic}"
+
+        except Exception as e:
+            print(e)
+            raise e
+
+        self.buffer = []
